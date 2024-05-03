c4ddf44c6d055fb0c2723a76652f7d54d4a6d13f
AndrewSalygin <andrew.salygin@gmail.com>
2024-05-03 11:55:49 +0400

    Create db scheme

--- migrations/sql/init-scheme.sql
+++ migrations/sql/init-scheme.sql
@@ -1,10 +1,51 @@
 --liquibase formatted sql
 
 --changeset hackathon:1
-CREATE TABLE IF NOT EXISTS Users (
+CREATE TYPE role AS ENUM ('user', 'admin');
+
+CREATE TABLE Users (
     user_id SERIAL PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    role role NOT NULL,
+    organization VARCHAR(255) NULL
 );
 
---rollback drop table Users;
+CREATE TABLE Status (
+    status_id SERIAL PRIMARY KEY,
+    status_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Card (
+    card_id SERIAL PRIMARY KEY,
+    complexity INT ,
+    comment VARCHAR(255),
+    photo TEXT,
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
+    points INT,
+    status_id INT REFERENCES Status(status_id)
+);
+
+CREATE TABLE Cleaning (
+    cleaning_id SERIAL PRIMARY KEY,
+    card_id INT REFERENCES Card(card_id),
+    status_id INT REFERENCES Status(status_id),
+    time TIMESTAMP
+);
+
+CREATE TABLE RequestStatusPerson (
+     request_id SERIAL PRIMARY KEY,
+     person_id INT REFERENCES Users(user_id)
+);
+
+-- Заполнение таблицы статусов
+INSERT INTO Status (status_name) VALUES
+     ('created'),
+     ('in process'),
+     ('being reviewed'),
+     ('closed');
+
+--rollback drop table User, Status, Card, Cleaning;
