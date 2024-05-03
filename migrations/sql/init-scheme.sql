9e9bd9b99419946cc79f214bd4bdd59ae225e05d
AndrewSalygin <andrew.salygin@gmail.com>
2024-05-03 13:37:29 +0400

    Fix db scheme

--- migrations/sql/init-scheme.sql
+++ migrations/sql/init-scheme.sql
@@ -1,25 +1,24 @@
 --liquibase formatted sql
 
 --changeset hackathon:1
-CREATE TYPE role AS ENUM ('user', 'admin');
 
 CREATE TABLE Users (
-    user_id SERIAL PRIMARY KEY,
+    user_id BIGSERIAL PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(100) NOT NULL,
-    role role NOT NULL,
+    role VARCHAR(20) NOT NULL,
     organization VARCHAR(255) NULL
 );
 
 CREATE TABLE Status (
-    status_id SERIAL PRIMARY KEY,
+    status_id BIGSERIAL PRIMARY KEY,
     status_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Card (
-    card_id SERIAL PRIMARY KEY,
+    card_id BIGSERIAL PRIMARY KEY,
     complexity INT ,
     comment VARCHAR(255),
     photo TEXT,
@@ -30,15 +29,11 @@ CREATE TABLE Card (
 );
 
 CREATE TABLE Cleaning (
-    cleaning_id SERIAL PRIMARY KEY,
-    card_id INT REFERENCES Card(card_id),
+    cleaning_id BIGSERIAL PRIMARY KEY,
+    card_id INT REFERENCES Card(card_id) NOT NULL,
     status_id INT REFERENCES Status(status_id),
+    user_id INT REFERENCES Users(user_id) NULL,
     time TIMESTAMP
-);
-
-CREATE TABLE RequestStatusPerson (
-     request_id SERIAL PRIMARY KEY,
-     person_id INT REFERENCES Users(user_id)
 );
 
 -- Заполнение таблицы статусов
