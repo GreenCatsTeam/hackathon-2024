a506a0cacf0baf8984605a595e402095258a262b
AndrewSalygin <andrew.salygin@gmail.com>
2024-05-04 14:55:31 +0400

    Security for dev

--- hackathon/src/main/java/com/greencats/configuration/security/SecurityConfig.java
+++ hackathon/src/main/java/com/greencats/configuration/security/SecurityConfig.java
@@ -1,61 +1,60 @@
-package com.greencats.configuration.security;//package com.greencats.configuration.security;
-
-import com.greencats.configuration.security.filters.JWTFilter;
-import com.greencats.security.UserDetailsServiceImpl;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-@Configuration
-@RequiredArgsConstructor
-public class SecurityConfig {
-
-    private final UserDetailsServiceImpl userDetailsService;
-
-    private final JWTFilter jwtFilter;
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(
-                auth -> auth
-                    .requestMatchers(HttpMethod.POST, "/auth/login", "/auth/registration").permitAll()
-                    .anyRequest().authenticated())
-            .userDetailsService(userDetailsService)
-            .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-        return httpSecurity.build();
-    }
-}
-
-
-// БЕЗ АВТОРИЗАЦИИ
+package com.greencats.configuration.security;
+//
+//import com.greencats.configuration.security.filters.JWTFilter;
+//import com.greencats.security.UserDetailsServiceImpl;
 //import lombok.RequiredArgsConstructor;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 //import org.springframework.security.config.http.SessionCreationPolicy;
 //import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 //
 //@Configuration
 //@RequiredArgsConstructor
 //public class SecurityConfig {
 //
+//    private final UserDetailsServiceImpl userDetailsService;
+//
+//    private final JWTFilter jwtFilter;
+//
 //    @Bean
 //    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-//        httpSecurity.csrf(AbstractHttpConfigurer::disable);
-//
-//        // Configure HTTP security to permit all requests without any authentication
-//        httpSecurity.authorizeHttpRequests(auth -> auth
-//                .anyRequest().permitAll())
-//            .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-//
+//        httpSecurity.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(
+//                auth -> auth
+//                    .requestMatchers(HttpMethod.POST, "/auth/login", "/auth/registration").permitAll()
+//                    .anyRequest().authenticated())
+//            .userDetailsService(userDetailsService)
+//            .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 //        return httpSecurity.build();
 //    }
 //}
+
+
+// БЕЗ АВТОРИЗАЦИИ
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        httpSecurity.authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll())
+            .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return httpSecurity.build();
+    }
+}
